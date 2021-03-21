247e08f94c93cecb31a354d9cf29c28032aaab6d
Rafael Rodríguez Puente <rafael@MacBook-Pro.local>
2021-03-21 11:16:25 +0200

    comments added

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,13 +1,16 @@
-
+# return the factorial of n
 def factorial(n):
     f = 1
     for i in range (1,n+1):
         f = f * i
     return f
-
+#return the binomial coeficient
 def binomial_coeficient(n, k):
     return factorial(n)/(factorial(k)*factorial(n-k))
 
+# here I will asume that the bigest value we are going to generate for the
+#triangle has three digits. 
+space = '   '
 def add_spaces_to_val(val):
     sval = str(val)
     if len(sval) == 1:
@@ -17,15 +20,16 @@ def add_spaces_to_val(val):
     return sval
 
 def print_pascal_triangle(n):
-    space = '   '
-    
     for row in range(n):
         # row i have i+1 cols
         left_spaces = space * (n-row)
+        # add spaces at the begening of the row to have the 
+        #shape of the triangle
         print(left_spaces, end=space)
         for col in range(row+1):
             #calculate the value for col 
             val = int(binomial_coeficient(row, col))
+            #adjust the number to three spaces
             print ( add_spaces_to_val(val), end=space)
         print('') # change to the new line
 
